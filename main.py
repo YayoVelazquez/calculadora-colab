8ed29fa184102107e3f75cf1caf762e181cb45f7
Yael Velazquez <yayo.velazquez@outlook.com>
2025-04-12 01:00:04 -0600

    Agrega función division

--- main.py
+++ main.py
@@ -4,6 +4,12 @@ def suma(a, b):
 def resta(a, b):
     return a - b
 
+def multiplicacion(a, b):
+    return a * b
+
+def division(a, b):
+    return a / b if b != 0 else "Error: división por cero"
 
 
 
+    
